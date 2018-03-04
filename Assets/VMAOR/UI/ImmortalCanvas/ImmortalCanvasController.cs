40ad32d493475aa648a93463791d1d37fb99d53e
Vincent Wing <vincentwing00@gmail.com>
2018-03-04 11:54:56 -0800

    Added scene selection menu.

---
+++ Assets/VMAOR/UI/ImmortalCanvas/ImmortalCanvasController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImmortalCanvasController : MonoBehaviour
+{
+    public Button quitButton;
+    public static bool didLoadOnce = false;
+
+    private void Start()
+    {
+        if (didLoadOnce)
+            Destroy(gameObject);
+        else
+        {
+            quitButton.onClick.AddListener(QuitButtonClick);
+            DontDestroyOnLoad(gameObject);
+            didLoadOnce = true;
+        }
+    }
+
+    private void QuitButtonClick()
+    {
+        if(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Application.Quit();
+        }
+        else
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+    }
+    private void OnApplicationQuit()
+    {
+        didLoadOnce = false;
+    }
+}
---
+++ Assets/_ARCore/Scripts/SceneSelectionMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneSelectionMenu : MonoBehaviour
+{
+    public Button scene1Button;
+    public Button scene2Button;
+
+    private void Start()
+    {
+        scene1Button.onClick.AddListener(delegate { UnityEngine.SceneManagement.SceneManager.LoadScene(1); });
+        scene2Button.onClick.AddListener(delegate { UnityEngine.SceneManagement.SceneManager.LoadScene(2); });
+    }
+}
